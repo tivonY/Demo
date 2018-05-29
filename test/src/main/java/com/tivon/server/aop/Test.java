9633aa2d61c7ca97bcfd852d38391b181951f0b7
tivonY <y_tivon@163.com>
2018-05-29 10:48:24 +0800

    update test

---
+++ test/src/main/java/com/tivon/server/aop/Test.java
@@ -0,0 +1,13 @@
+package com.tivon.server.aop;
+
+public class Test {
+
+//	AOP 资料：
+//	http://blog.csdn.net/h_xiao_x/article/details/72774496
+//	https://www.jianshu.com/p/f51bdde15191
+//	https://www.cnblogs.com/gaopeng527/p/5290997.html
+	
+	
+	
+	
+}
---
+++ test/src/main/java/com/tivon/server/sql/SqlMain.java
@@ -0,0 +1,18 @@
+package com.tivon.server.sql;
+
+public class SqlMain {
+
+	
+	public static void main(String[] args) {
+		
+		//sql server
+		//uuid : newid();
+		//1.修改表字段：ALTER TABLE t_user ALTER COLUMN upass VARCHAR(255);
+		//2.datetime and datetime2 ： datetime2精确度高； 
+		//3.datetimeoffset - 时区
+		//4. timestamp 是二进制数字，它表明数据库中数据修改发生的相对顺序;最初是为了支持 SQL Server 恢复算法
+		
+		
+	}
+	
+}
